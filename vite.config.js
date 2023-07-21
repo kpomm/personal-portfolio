f15ef5e72da491f9b4daf36600d93ac7eaffe395
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-07-20 21:16:08 -0400

    favicon

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 // eslint-disable-next-line import/no-unresolved
 import eslint from 'vite-plugin-eslint';
 import autoprefixer from 'autoprefixer';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 // followed this documentation to add favicon plugin: https://github.com/JohnPremKumar/vite-plugin-favicons-inject
 
 // https://vitejs.dev/config/
