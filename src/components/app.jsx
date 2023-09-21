b532d761bcf78ef7b8108aa1d41489e4ddd15cda
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-09-21 13:25:31 -0400

    cleaning up extra commented out code

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -9,38 +9,7 @@ import Photos from './Photos';
 import Experience from './Experience';
 import Projects from './Projects';
 import ContactMe from './ContactMe';
-import Apod from './Apod';
 import Particle from './Particle';
-
-// function Test(props) {
-//   const { id } = useParams();
-//   return (
-//     <div>
-//       {' '}
-//       ID:
-//       {' '}
-//       {id}
-//       {' '}
-//     </div>
-//   );
-// }
-
-// function FallBack(props) {
-//   return <div>URL Not Found</div>;
-// }
-
-// function Nav(props) {
-//   return (
-//     <nav>
-//       <ul>
-//         <li><NavLink to="/">Home</NavLink></li>
-//         <li><NavLink to="/about">About</NavLink></li>
-//         <li><NavLink to="/test/id1">test id1</NavLink></li>
-//         <li><NavLink to="/test/id2">test id2</NavLink></li>
-//       </ul>
-//     </nav>
-//   );
-// }
 
 // app
 function App(props) {
@@ -58,7 +27,6 @@ function App(props) {
             <Route path="/experience" element={<Experience />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact-me" element={<ContactMe />} />
-            <Route path="/apod" element={<Apod />} />
           </Routes>
         </div>
       </BrowserRouter>
