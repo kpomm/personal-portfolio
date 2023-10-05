4ffb2081bfe7552872258ede0a883719400a9b66
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-10-05 14:55:49 -0400

    cutesy contact me page!

--- src/components/ContactMePage.jsx
+++ src/components/ContactMePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContactMe from './ContactMe';
 import contactGraphic from '../img/contactGraphic.svg'; // Tell webpack this JS file uses this image
+import Divider from './Divider';
 
 function ContactMePage() {
   return (
     <div id="contact-me-fullscreen">
       <img src={contactGraphic} alt="" />
       reach out!
+      <Divider />
       <ContactMe />
     </div>
   );
